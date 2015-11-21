5b38c18bb9831857f609c8fe74ad822fbc7c7a22
zabaanto <antonin.zaba@centrum.cz>
2015-11-21 16:33:00 +0100

    #1 - priklad staticke DB

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -12,10 +12,31 @@ use App\Model;
 
 class HomepagePresenter extends BasePresenter {
 
-	public function renderDefault() {	
+	public function renderDefault() {
 		// vytvorim objekt pomoci me modelove tridy v app/model
 		$mujZooObjekt = new Model\TestModelClass;
 		// do promenne pro template nactu pole zvirat
-		$this->template->zviratka = $mujZooObjekt->vratPole(); 
+		$this->template->zviratka = $mujZooObjekt->vratPole();
+	}
+
+	public function renderDefaultAlternative() {
+		/* staticka data z DB */
+		$inzeraty = array(
+			12 => array( // ID inzeratu
+				'header' => 'Prodam ledničku s mrazákem',
+				'body' => 'Lednicka koupena roku 2004. Narocne pouzivana. Fungujici',
+				'prize' => 1800,
+				'pictures' => array('111.png', '123.jpg', '124.png'), // seznam obrazku, ktery jsou ulozeny na serveru.. obrazky budou na serveru ulozeny pod ID, nikoliv pod svym jmenem
+				),
+			33 => array(
+				'header' => 'Prodam ledničku s mrazákem',
+				'body' => 'Lednicka koupena roku 2008. Narocne pouzivana. Fungujici',
+				'prize' => 500,
+				'pictures' => array(), // nekde obrazky nebudou vubec => zobrazit cast body
+				),
+			);
+
+		$this->template->inzeraty = $inzeraty;
 	}
 }
+
