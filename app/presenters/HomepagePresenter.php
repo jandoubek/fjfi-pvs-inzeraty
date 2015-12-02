45ced1beb437520783132970009bbe4b7bdc26e7
hubintom <tomas@Tomas-MacBook-Air.local>
2015-12-02 12:16:47 +0100

    #2 Cleaning directories

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -29,34 +29,8 @@ class HomepagePresenter extends BasePresenter {
 		$mujZooObjekt = new Model\TestModelClass;
 		// do promenne pro template nactu pole zvirat
 		$this->template->zviratka = $mujZooObjekt->vratPole();
-	}
 
-	public function renderDefaultAlternative() {
-		/* staticka data z DB */
-		$inzeraty = array(
-			12 => array( // ID inzeratu
-				'header' => 'Prodam ledničku s mrazákem',
-				'body' => 'Lednicka koupena roku 2004. Narocne pouzivana. Fungujici',
-				'prize' => 1800,
-				'pictures' => array('111.png', '123.jpg', '124.png'), // seznam obrazku, ktery jsou ulozeny na serveru.. obrazky budou na serveru ulozeny pod ID, nikoliv pod svym jmenem
-				),
-			33 => array(
-				'header' => 'Prodam ledničku s mrazákem',
-				'body' => 'Lednicka koupena roku 2008. Narocne pouzivana. Fungujici',
-				'prize' => 500,
-				'pictures' => array(), // nekde obrazky nebudou vubec => zobrazit cast body
-				),
-			);
-		//Zda je uzivatel prihlasen ci ne
-		$prihlasen = true;
-
-		$this->template->inzeraty = $inzeraty;
-
-		$this->template->prihlasen = $prihlasen;
-	}
-
-	public function renderDefaultAlternative2() {
-		/* staticka data z DB */
+				/* staticka data z DB */
 		$inzeraty = array(
 			12 => array( // ID inzeratu
 				'header' => 'Prodam ledničku s mrazákem',
