c0a6eb92f997782bcf3b5e85ca55b09f17a1ab86
zabaanto <antonin.zaba@centrum.cz>
2015-12-20 12:20:06 +0100

    #23 - inzerat - odkaz na autora

--- www/assets/js/main_page/main_page.js
+++ www/assets/js/main_page/main_page.js
@@ -1,7 +1,8 @@
+$(document).on('hidden.bs.modal', '#inzerat_modal, #profile_modal, #inzerat_profile_modal', function(){
+	 $(this).data('bs.modal', null);
+})
+
 $(document).ready(function() {
-		$('#inzerat_modal').on('hidden.bs.modal', function(){
-    	$(this).data('bs.modal', null);
-		});
 		$('.carousel').carousel({
       		interval: 3000
     })
