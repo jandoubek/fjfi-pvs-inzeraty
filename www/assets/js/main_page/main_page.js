c63350d38cfa72e4ef8ed9e51cc4926c101da2eb
zabaanto <antonin.zaba@centrum.cz>
2016-01-05 09:15:25 +0100

    Manualni popUp Inzeratu
    + duvodem byl nehezky reload obsahu popUpu

--- www/assets/js/main_page/main_page.js
+++ www/assets/js/main_page/main_page.js
@@ -1,5 +1,16 @@
-$(document).on('hidden.bs.modal', '#inzerat_modal, #profile_modal, #inzerat_profile_modal', function(){
-	 $(this).data('bs.modal', null);
+$(document).on('click', 'a[data-target=#inzerat_modal]', function(ev) {
+	ev.preventDefault();
+  var target = $(this).attr("href");
+
+  // load the url and show modal on success
+  $("#myModal .modal-content").load(target, function() {
+       $("#myModal").modal("show");
+       $("#myModal").children('.modal-dialog').css('width', '800px');
+  });
+});
+
+$(document).on('hidden.bs.modal', '#profile_modal, #inzerat_profile_modal', function(){
+	$(this).data('bs.modal', null);
 })
 
 $(document).ready(function() {
